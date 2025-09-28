9d7c63583770cf8490d2e85a73b4c53ea4e5c98e
Ayushshedge <ayushshedge9090@gmail.com>
2025-09-28 12:46:38 +0530

    Update bubble.cpp

--- bubble.c
+++ bubble.c
@@ -1,40 +1,73 @@
 // Write a program that demonstrates how to organize (sort)
 // cards in a hand using insertion sort.
 // Ayush shedge (B24CE1108)
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
-    int n = 0;
-    printf("Enter the number of cards: ");
-    scanf("%d", &n);
-
-    int cards[n];
-    printf("Enter the card values: ");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &cards[i]);
+    int score1[5];  
+    cout << "Enter for Player 1:\n";
+    for (int i = 0; i < 5; i++) {
+        cin >> score1[i];
     }
 
-    printf("Original hand: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", cards[i]);
-    }
-    printf("\n");
-
-    // Insertion Sort
-    for (int i = 1; i < n; i++) {
-        int key = cards[i];
-        int j;
-        for (j = i - 1; j >= 0 && cards[j] > key; j--) {
-            cards[j + 1] = cards[j];
+    for (int i = 0; i < 4; i++) {           
+        for (int j = 0; j < 4 - i; j++) {   
+            if (score1[j] < score1[j + 1]) { 
+                int temp = score1[j];
+                score1[j] = score1[j + 1];
+                score1[j + 1] = temp;
+            }
         }
-        cards[j + 1] = key;
     }
 
-    printf("Sorted hand: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", cards[i]);
+    
+    cout << "\nPlayer 1 score in descending order:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << score1[i] << " ";
     }
-    printf("\n");
 
-return 0;
+    
+    cout << endl;
+
+
+     int score2[5];  
+    cout << "Enter score for Player 2:\n";
+    for (int i = 0; i < 5; i++) {
+        cin >> score2[i];
+    }
+
+    for (int i = 0; i < 4; i++) {           
+        for (int j = 0; j < 4 - i; j++) {   
+            if (score2[j] < score2[j + 1]) { 
+                int temp = score2[j];
+                score2[j] = score2[j + 1];
+                score2[j + 1] = temp;
+            }
+        }
+    }
+
+    
+    cout << "\nPlayer 2 score in descending order:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << score2[i] << " ";
+    }
+
+    cout << endl;
+
+    if(score1[0]>score2[0]){
+        cout<<"player 1 is winner";
+    }
+    
+    else if(score1[0]<score2[0]){
+        cout<<"player 2 is winner";
+    }
+
+    else{
+        cout<<"Match is tie";
+    }
+
+
+
+    return 0;
 }
